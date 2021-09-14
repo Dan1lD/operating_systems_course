e0e22922bc3f1dff8cd3fbac170c25512905c8f2
Dan1lD <davidian654@gmail.com>
2021-09-14 23:55:17 +0300

    lab 4

---
+++ week4/ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "unistd.h"
+
+int main() {
+    int n = 10;
+    int pid = fork();
+
+    if (pid == 0)
+        printf("Hello from child [%d - %d]\n", getpid(), n);
+    else if (pid >0)
+        printf("Hello from parent [%d - %d]\n", getpid(), n);
+    else return -1;
+
+    return 0;
+}
